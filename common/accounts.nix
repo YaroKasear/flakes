a7f48a5bb725d5b110b5ae5fe015409a3d4104b6
Yaro Kasear <yarokasear@gmail.com>
2023-11-30 18:18:38 -0600

    Add work email.

--- common/accounts.nix
+++ common/accounts.nix
@@ -1,15 +1,25 @@
 {
-    accounts.email.accounts = {
-        gmail = {
-            address = "yarokasear@gmail.com";
-            flavor = "gmail.com";
-            gpg = {
-                key = "8A676FDCAAD929184299D020151A8F0401FB2E85";
-                signByDefault = true;
-            };
-            primary = true;
-            realName = "Yaro Kasear";
-            thunderbird.enable = true;
-        };
+  accounts.email.accounts = {
+    Personal = {
+      address = "yarokasear@gmail.com";
+      flavor = "gmail.com";
+      gpg = {
+          key = "8A676FDCAAD929184299D020151A8F0401FB2E85";
+          signByDefault = true;
+      };
+      primary = true;
+      realName = "Yaro Kasear";
+      thunderbird.enable = true;
     };
+    Work = {
+      address = "cnelson@braunresearch.com";
+      flavor = "gmail.com";
+      gpg = {
+          key = "8A676FDCAAD929184299D020151A8F0401FB2E85";
+          signByDefault = false;
+      };
+      realName = "Conrad Nelson";
+      thunderbird.enable = true;
+    };
+  };
 }
