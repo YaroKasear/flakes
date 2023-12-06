e39e95943d85761e5e7addaaf305124ac56d73a6
Yaro Kasear <yarokasear@gmail.com>
2023-12-05 21:01:40 -0600

    No more experimenting with s3air for now.

--- custom/sonic3air/flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  outputs = {self, nixpkgs}: {
-    overlays.default = final: prev: { sonic3air = prev.pkgs.callPackage ./derivation.nix { }; };
-
-    packages.x86_64-linux.sonic3air = nixpkgs.legacyPackages.x86_64-linux.callPackage ./derivation.nix { };
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.sonic3air;
-  };
-}
