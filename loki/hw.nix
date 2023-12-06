8b130d798f3bfe552d710516495557944a215c69
Yaro Kasear <yarokasear@gmail.com>
2023-12-06 12:05:04 -0600

    Add my media.

--- loki/hw.nix
+++ loki/hw.nix
@@ -20,6 +20,24 @@
     fsType = "vfat";
   };
 
+  fileSystems."/mnt/music" = {
+    device = "storage.kasear.net:/mnt/data/storage/music";
+    fsType = "nfs";
+    options = [ "nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
+  };
+
+  fileSystems."/mnt/video" = {
+    device = "storage.kasear.net:/mnt/data/storage/video";
+    fsType = "nfs";
+    options = [ "nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
+  };
+
+  fileSystems."/mnt/pictures" = {
+    device = "storage.kasear.net:/mnt/data/storage/pictures";
+    fsType = "nfs";
+    options = [ "nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
+  };
+
   fileSystems."/mnt/games" = {
     device = "storage.kasear.net:/mnt/data/storage/games";
     fsType = "nfs";
