8a64ed90c9a0188cf76b46548affd7934134aa25
Yaro Kasear <yarokasear@gmail.com>
2023-12-13 22:42:17 -0600

    Tidy up the flake slightly.

--- flake.nix
+++ flake.nix
@@ -3,31 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-index-database = {
-      url = "github:Mic92/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+
     cowsay = {
       url = "github:snowfallorg/cowsay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     wallpaper-generator = {
       url = "github:pinpox/wallpaper-generator";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -51,27 +59,29 @@
         allowUnfree = true;
       };
 
-      systems.modules.nixos = with inputs; [
-        nixos-hardware.nixosModules.common-pc
-        sops-nix.nixosModules.sops
-      ];
+      systems = {
+        modules.nixos = with inputs; [
+          nixos-hardware.nixosModules.common-pc
+          sops-nix.nixosModules.sops
+        ];
+        hosts.loki.modules = with inputs; [
+          nixos-hardware.nixosModules.common-pc-ssd
+          nixos-hardware.nixosModules.common-cpu-amd
+          nixos-hardware.nixosModules.common-cpu-amd-pstate
+          nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+        ];
+      };
 
-      systems.hosts.loki.modules = with inputs; [
-        nixos-hardware.nixosModules.common-pc-ssd
-        nixos-hardware.nixosModules.common-cpu-amd
-        nixos-hardware.nixosModules.common-cpu-amd-pstate
-        nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
-      ];
-
-      homes.users."yaro@loki".modules = with inputs; [
-        nix-index-database.hmModules.nix-index
-        sops-nix.homeManagerModules.sops
-      ];
-
-      homes.users."yaro@Gwyn".modules = with inputs; [
-        nix-index-database.hmModules.nix-index
-        sops-nix.homeManagerModules.sops
-      ];
+      homes.users = {
+        "yaro@loki".modules = with inputs; [
+          nix-index-database.hmModules.nix-index
+          sops-nix.homeManagerModules.sops
+        ];
+        "yaro@Gwyn".modules = with inputs; [
+          nix-index-database.hmModules.nix-index
+          sops-nix.homeManagerModules.sops
+        ];
+      };
     };
 }
 
