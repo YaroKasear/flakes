920951bb4a13ab113bffdcc7453e2df2d80fe923
Yaro Kasear <yarokasear@gmail.com>
2023-12-17 22:24:04 -0600

    Fixed some tmux config oopsies.

--- modules/home/tmux/default.nix
+++ modules/home/tmux/default.nix
@@ -33,21 +33,12 @@ in {
           set -ga status-left '#[bg=${tertiary}]#[fg=${primary}]#{?client_prefix,#[bg=${tertiary}]#[fg=${red}],}'
           set -g status-justify absolute-centre
           set-window-option -g window-status-style bg='${secondary},fg=${primary}'
-          set-window-option -g window-status-current-style bg='${primary},fg=${secondary}'
+          set-window-option -g window-status-current-style bg='${primary}',fg='${secondary}'
           set -g window-status-current-format "#[fg=${primary}]#[bg=${tertiary}]#[fg=${secondary}]#[bg=${primary}] #I #W #[fg=${primary}]#[bg=${tertiary}]"
           set -g window-status-format "#[fg=${secondary}]#[bg=${tertiary}]#[fg=${primary}]#[bg=${secondary}] #I #W #[bg=${tertiary}]#[fg=${secondary}]"
           set -g status-right '#[bg=${tertiary}]#[fg=${primary}]'
           set -ga status-right '#[bg=${primary}]#[fg=${secondary}] %a %H:%M:%S %Y-%m-%d #[bg=${tertiary}]#[fg=${primary}]'
         '';
-          # set -g status-style bg='${primary}',fg='${secondary}'
-          # set -g status-left '#[bg=${primary}]#[fg=${secondary}]#{?client_prefix,#[bg=${secondary}]#[fg=${primary}],} ☺ '
-          # set -ga status-left '#[bg=${primary}]#[fg=${secondary}] #{?window_zoomed_flag, ↕  ,   }'
-          # set-window-option -g window-status-style fg='${secondary}',bg=default
-          # set-window-option -g window-status-current-style fg='${primary}',bg='${secondary}'
-          # set -g window-status-current-format "#[fg=${primary}]#[bg=${secondary}]#[fg=]#[bg=${secondary}] #I #W #[fg=${secondary}]#[bg=${primary}]"
-          # set -g window-status-format "#[fg=${tertiary}]#[bg=${primary}]#I #W #[fg=${primary}] "
-          # set -g status-right '#[fg=${secondary},bg=${primary}]#[fg=${primary},bg=${secondary}] #(uptime | cut -f 4-6 -d " " | cut -f 1 -d ",") '
-          # set -ga status-right '#[fg=${primary},bg=${secondary}] %a %H:%M:%S %Y-%m-%d'
       };
     };
   };
