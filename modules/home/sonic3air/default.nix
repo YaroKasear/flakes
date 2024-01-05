6875e4a4adc9bc9e149f89f70e5b27d98ad3976e
Yaro Kasear <yarokasear@gmail.com>
2024-01-05 14:45:34 -0600

    Fri Jan  5 02:45:34 PM CST 2024

--- modules/home/sonic3air/default.nix
+++ modules/home/sonic3air/default.nix
@@ -10,21 +10,24 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.file.s3air-config = {
-      source = ../../../files/sonic3air/settings.json;
-      target = ".config/Sonic3AIR/settings.json";
-    };
+    # home.file.s3air-config = {
+    #   source = ../../../files/sonic3air/settings.json;
+    #   target = ".config/Sonic3AIR/settings.json";
+    # };
 
-    xdg.desktopEntries = {
-      sonic3air = {
-        name = "Sonic 3: Angel Island Revisited";
-        genericName = "Sonic Fan Remaster of Sonic 3 & Knuckles";
-        type = "Application";
-        exec = "steam-run /mnt/games/Sonic\\ 3:\\ Angel\\ Island\\ Revisited/sonic3air_linux";
-        terminal = false;
-        categories = [ "Game" ];
-        icon = "/mnt/games/Sonic\\ 3:\\ Angel\\ Island\\ Revisited/sonic3air_linux/data/icon.png";
+    xdg = {
+      desktopEntries = {
+        sonic3air = {
+          name = "Sonic 3: Angel Island Revisited";
+          genericName = "Sonic Fan Remaster of Sonic 3 & Knuckles";
+          type = "Application";
+          exec = "steam-run /mnt/games/Sonic\\ 3:\\ Angel\\ Island\\ Revisited/sonic3air_linux";
+          terminal = false;
+          categories = [ "Game" ];
+          icon = "/mnt/games/Sonic\\ 3:\\ Angel\\ Island\\ Revisited/sonic3air_linux/data/icon.png";
+        };
       };
+      configFile."Sonic3AIR/settings.json".source = ./files/settings.json;
     };
   };
 }
