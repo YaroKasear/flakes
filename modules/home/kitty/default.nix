10b158a5f3230ebb2acfaf04a4551dd094bcf425
Yaro Kasear <yarokasear@gmail.com>
2024-01-08 09:47:52 -0600

    Mon Jan  8 09:47:52 AM CST 2024

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -80,7 +80,7 @@ in {
           tab_bar_margin_width 9
           tab_bar_margin_height 9 0
           tab_bar_style separator
-          tab_bar_min_tabs 1
+          tab_bar_min_tabs 2
           tab_separator ""
           tab_title_template "{fmt.fg.${inactiveBackgroundColor}}{fmt.bg.default}{fmt.fg.${inactiveForegroundColor}}{fmt.bg.${inactiveBackgroundColor}}{fmt.bold} {title.split()[0]} {fmt.fg.${inactiveBackgroundColor}}{fmt.bg.default} "
           active_tab_title_template "{fmt.fg.${activeBackgroundColor}}{fmt.bg.default}{fmt.fg.${activeForegroundColor}}{fmt.bg.${activeBackgroundColor}}{fmt.bold} {title.split()[0]} {fmt.fg.${activeBackgroundColor}}{fmt.bg.default} "
