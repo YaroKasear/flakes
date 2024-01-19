3e769fa0f00d6150218644d00e910b970fffac67
Yaro Kasear <yarokasear@gmail.com>
2024-01-19 11:26:42 -0600

    Fri Jan 19 11:26:42 AM CST 2024

--- modules/home/gui/hyprland/default.nix
+++ modules/home/gui/hyprland/default.nix
@@ -200,6 +200,7 @@ in {
         misc = {
           background_color = mkDefault "$background";
           disable_hyprland_logo = true;
+          disable_splash_rendering = true;
           vrr = 2;
         };
 
