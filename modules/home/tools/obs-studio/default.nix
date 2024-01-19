a8d903b552e9908de9219094b7f0c08cea8d2d03
Yaro Kasear <yarokasear@gmail.com>
2024-01-18 21:53:31 -0600

    Thu Jan 18 09:53:31 PM CST 2024

---
+++ modules/home/tools/obs-studio/default.nix
@@ -0,0 +1,15 @@
+{ lib, config, pkgs, inputs, ... }:
+
+with lib;
+with lib.united;
+let
+  cfg = config.united.obs-studio;
+in {
+  options.united.obs-studio = {
+    enable = mkEnableOption "OBS Studio";
+  };
+
+  config = mkIf cfg.enable {
+    programs.obs-studio.enable = true;
+  };
+}
