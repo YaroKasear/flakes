e1b0daffa388c58cbbbb85527e9e588ff923c7f3
Yaro Kasear <yarokasear@gmail.com>
2024-01-25 15:06:31 -0600

    Thu Jan 25 03:06:31 PM CST 2024

--- modules/home/gui/ags/default.nix
+++ modules/home/gui/ags/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, pkgs, inputs, ... }:
 
 with lib;
 with lib.united;
@@ -12,11 +12,16 @@ in {
   config = mkIf cfg.enable {
     programs.ags = {
       enable = true;
+      package = inputs.ags.packages.${pkgs.system}.agsWithTypes;
     };
 
     xdg.configFile = {
       "ags" = {
         source = ./files;
+        recursive = true;
+      };
+      "ags/types" = {
+        source = "${inputs.ags.packages.${pkgs.system}.agsWithTypes}/share/com.github.Aylur.ags/types";
         recursive = true;
       };
     };
