fd0159b61d9b6a1670ccd5174984f0cde971bd16
Yaro Kasear <yarokasear@gmail.com>
2024-02-11 08:22:34 -0600

    Sun Feb 11 08:22:34 AM CST 2024

--- modules/nixos/protomuck/default.nix
+++ modules/nixos/protomuck/default.nix
@@ -3,10 +3,10 @@
 with lib;
 with lib.united;
 let
-  cfg = config.united.media-mounts;
+  cfg = config.united.protomuck;
 in {
-  options.united.media-mounts = rec {
-    enable = mkEnableOption "Media-mounts";
+  options.united.protomuck = rec {
+    enable = mkEnableOption "Protomuck";
     game-name = mkOpt types.string "ProtoMUCK" "Name of the MUCK server.";
     game-directory = mkOpt types.path "/srv/${game-name}" "Location of the MUCK files.";
     main-port = mkOpt types.port 8881 "Main port of the MUCK.";
