bd30042edeb636f0d30cbde95998bb91d2c47195
Yaro Kasear <yarokasear@gmail.com>
2024-02-12 21:03:07 -0600

    Mon Feb 12 21:03:07 CST 2024

--- systems/aarch64-darwin/Gwyn/default.nix
+++ systems/aarch64-darwin/Gwyn/default.nix
@@ -3,7 +3,18 @@
 {
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = "nix-command flakes";
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = "nix-command flakes";
+    };
+    gc = {
+      automatic = true;
+      interval.Day = 7;
+    };
+    extraOptions = ''
+      min-free = ${toString (1024 * 1024 * 1024)}
+      max-free = ${toString (5 * 1024 * 1024 * 1024)}
+    '';
   };
 
   nixpkgs.hostPlatform = "aarch64-darwin";
