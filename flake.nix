de101d44eced4d0cab13dd195c8b910482345b3e
Yaro Kasear <yarokasear@gmail.com>
2024-02-13 20:18:27 -0600

    Tue Feb 13 08:18:27 PM CST 2024

--- flake.nix
+++ flake.nix
@@ -50,6 +50,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
