e1887c2660a1350c3e81ec87e459451aaaefee0c
Yaro Kasear <yarokasear@gmail.com>
2024-02-16 11:41:57 -0600

    Fri Feb 16 11:41:57 AM CST 2024

---
+++ modules/home/cli/zoxide/default.nix
@@ -0,0 +1,17 @@
+{ lib, config, pkgs, ... }:
+
+with lib;
+with lib.united;
+let
+  cfg = config.united.zoxide;
+in {
+  options.united.zoxide = {
+    enable = mkEnableOption "Zoxide";
+  };
+
+  config = mkIf cfg.enable {
+    programs.zoxide = {
+      enable = true;
+    };
+  };
+}
