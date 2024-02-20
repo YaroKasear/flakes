aafc6a02647223ecab54ed8b8fdb597eb95f4645
Yaro Kasear <yarokasear@gmail.com>
2024-02-19 22:46:47 -0600

    Mon Feb 19 10:46:47 PM CST 2024

--- modules/home/tools/bat/default.nix
+++ modules/home/tools/bat/default.nix
@@ -10,16 +10,22 @@ in {
   };
 
   config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      entr
+    ];
+
     programs = {
       bat = {
         enable = true;
         extraPackages = with pkgs.bat-extras; [
           batman
+          batwatch
         ];
       };
       zsh.shellAliases = mkIf config.united.zsh.enable {
         cat = "bat";
         man = "batman";
+        watch = "batwatch";
       };
     };
   };
