3b853dcf35cb62a9a52b98c9870cc4914967ff97
Yaro Kasear <yarokasear@gmail.com>
2024-02-26 09:09:47 -0600

    Mon Feb 26 09:09:47 AM CST 2024

--- flake.nix
+++ flake.nix
@@ -181,13 +181,6 @@
           nixos-hardware.nixosModules.common-cpu-amd-pstate
           nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
         ];
-        hosts.experimental.modules = with inputs; [
-          nixos-hardware.nixosModules.common-pc
-          nixos-hardware.nixosModules.common-pc-ssd
-          nixos-hardware.nixosModules.common-cpu-amd
-          nixos-hardware.nixosModules.common-cpu-amd-pstate
-          nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
-        ];
       };
 
       homes.users = {
