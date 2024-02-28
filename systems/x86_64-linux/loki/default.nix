7fac650edc9dca1ac61611aa8cf3469783168b7a
Yaro Kasear <yarokasear@gmail.com>
2024-02-28 10:55:33 -0600

    Wed Feb 28 10:55:33 AM CST 2024

--- systems/x86_64-linux/loki/default.nix
+++ systems/x86_64-linux/loki/default.nix
@@ -18,36 +18,36 @@
     };
   };
 
-  # sops = {
-  #   secrets = {
-  #     cnelson-authfile = {
-  #       path = "/home/cnelson/.config/Yubico/u2f_keys";
-  #       mode = "0440";
-  #       owner = config.users.users.cnelson.name;
-  #       group = config.users.users.cnelson.group;
-  #       sopsFile = ./secrets.yaml;
-  #     };
-  #   };
-  # };
+  sops = {
+    secrets = {
+      cnelson-authfile = {
+        path = "/home/cnelson/.config/Yubico/u2f_keys";
+        mode = "0440";
+        owner = config.users.users.cnelson.name;
+        group = config.users.users.cnelson.group;
+        sopsFile = ./secrets.yaml;
+      };
+    };
+  };
 
-  # systemd = {
-  #   tmpfiles.settings = {
-  #     "10-fix-dotconfig" = {
-  #       "/home/cnelson/.config" = {
-  #         d = {
-  #           user = "cnelson";
-  #           group = "users";
-  #           mode = "0755";
-  #         };
-  #       };
-  #       "/home/cnelson/.config/Yubico" = {
-  #         d = {
-  #           user = "cnelson";
-  #           group = "users";
-  #           mode = "0755";
-  #         };
-  #       };
-  #     };
-  #   };
-  # };
+  systemd = {
+    tmpfiles.settings = {
+      "10-fix-dotconfig" = {
+        "/home/cnelson/.config" = {
+          d = {
+            user = "cnelson";
+            group = "users";
+            mode = "0755";
+          };
+        };
+        "/home/cnelson/.config/Yubico" = {
+          d = {
+            user = "cnelson";
+            group = "users";
+            mode = "0755";
+          };
+        };
+      };
+    };
+  };
 }
