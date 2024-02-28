08eae117598dd72b7929566ceaf9841b2d8df222
Yaro Kasear <yarokasear@gmail.com>
2024-02-28 10:49:50 -0600

    Wed Feb 28 10:49:50 AM CST 2024

--- systems/x86_64-linux/loki/default.nix
+++ systems/x86_64-linux/loki/default.nix
@@ -8,4 +8,46 @@
       use-wayland = true;
     };
   };
+
+  users = {
+    users.cnelson = {
+      isNormalUser = true;
+      extraGroups = ["video" "audio" "lp"];
+      shell = pkgs.zsh;
+      hashedPasswordFile = config.sops.secrets.hashedpw.path;
+    };
+  };
+
+  # sops = {
+  #   secrets = {
+  #     cnelson-authfile = {
+  #       path = "/home/cnelson/.config/Yubico/u2f_keys";
+  #       mode = "0440";
+  #       owner = config.users.users.cnelson.name;
+  #       group = config.users.users.cnelson.group;
+  #       sopsFile = ./secrets.yaml;
+  #     };
+  #   };
+  # };
+
+  # systemd = {
+  #   tmpfiles.settings = {
+  #     "10-fix-dotconfig" = {
+  #       "/home/cnelson/.config" = {
+  #         d = {
+  #           user = "cnelson";
+  #           group = "users";
+  #           mode = "0755";
+  #         };
+  #       };
+  #       "/home/cnelson/.config/Yubico" = {
+  #         d = {
+  #           user = "cnelson";
+  #           group = "users";
+  #           mode = "0755";
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 }
