2b8e913b7110706a6f3780c32db463f8518b1499
Yaro Kasear <yarokasear@gmail.com>
2024-03-17 09:36:50 -0500

    Sun Mar 17 09:36:50 AM CDT 2024

--- modules/nixos/wayland/default.nix
+++ modules/nixos/wayland/default.nix
@@ -8,15 +8,34 @@ in {
   options.united.wayland = {
     enable = mkEnableOption "Wayland";
     greeter = mkEnableOption "Use greeter";
+    compositor = mkOption {
+      type = types.enum [
+        "hyprland"
+        "plasma"
+      ];
+      default = "hyprland";
+      description = "Select compositor!";
+    };
   };
 
   config = mkIf cfg.enable {
     united.greetd.enable = cfg.greeter;
 
     programs.hyprland = {
-      enable = true;
+      enable = cfg.compositor == "hyprland";
       package = inputs.hyprland.packages.${pkgs.system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+    };
+
+    services = mkIf (cfg.compositor == "plasma") {
+      xserver = {
+        enable = true;
+        displayManager = {
+          sddm.enable = true;
+          defaultSession = "plasma";
+        };
+      };
+      desktopManager.plasma6.enable = true;
     };
   };
 }
