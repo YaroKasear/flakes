e508c01ae7e0034e9e6a8c1df0cc7046c5bcdd0c
Yaro Kasear <yarokasear@gmail.com>
2024-06-02 14:15:17 -0500

    Sun Jun  2 14:15:17 2024

--- flake.nix
+++ flake.nix
@@ -261,6 +261,7 @@
           nix-index-database.hmModules.nix-index
           nixvim.homeManagerModules.nixvim
           nur.hmModules.nur
+          plasma-manager.homeManagerModules.plasma-manager
           sops-nix.homeManagerModules.sops
         ];
         "yaro@Gwyn".modules = with inputs; [
