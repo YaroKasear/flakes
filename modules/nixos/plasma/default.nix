5eb5da276dbbbf5d7f7277da6d6406bf2501ea59
Yaro Kasear <yarokasear@gmail.com>
2024-06-04 09:11:19 -0500

    Tue Jun  4 09:11:19 2024

--- modules/nixos/plasma/default.nix
+++ modules/nixos/plasma/default.nix
@@ -10,19 +10,42 @@ in {
   };
 
   config = mkIf cfg.enable {
-    # environment.systemPackages = with pkgs; [catppuccin-sddm-corners];
+    # Based on answer given at https://www.reddit.com/r/NixOS/comments/1c22yfj/how_to_change_the_sddm_user_avatar/
+    boot.postBootCommands = ''
+      ${concatMapStringsSep
+        "\n"
+        (user: let
+          configDest = "/var/lib/AccountsService/users/${user.united.user.name}";
+          iconDest = "/var/lib/AccountsService/icons/${user.united.user.name}";
+          userConf = ''
+            [User]
+            Session=
+            XSession=
+            Icon=${iconDest}
+            SystemAccount=false
+          ''; in ''
+          mkdir -p /var/lib/AccountsService/users/
+          mkdir -p /var/lib/AccountsService/icons/
+          cp -r  ${user.united.user.icon} ${iconDest}
+          echo '${userConf}' > ${configDest}
+        '')
+        (builtins.attrValues config.home-manager.users)
+      }
+    '';
+
+    environment.systemPackages = with pkgs; [catppuccin-sddm-corners];
 
     services = {
         displayManager = {
           sddm = {
             enable = true;
-            # package = mkForce pkgs.libsForQt5.sddm;
+            package = mkForce pkgs.libsForQt5.sddm;
             wayland = enabled;
             settings = {
               General.Numlock = "on";
             };
-            # extraPackages = pkgs.lib.mkForce [ pkgs.libsForQt5.qt5.qtgraphicaleffects ];
-            # theme = "catppuccin-sddm-corners";
+            extraPackages = pkgs.lib.mkForce [ pkgs.libsForQt5.qt5.qtgraphicaleffects ];
+            theme = "catppuccin-sddm-corners";
           };
           defaultSession = "plasma";
         };
