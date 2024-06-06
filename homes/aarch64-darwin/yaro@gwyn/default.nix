a0a1d573d9833b750785f623f50def8963808e25
Yaro Kasear <yarokasear@gmail.com>
2024-06-06 12:13:23 -0500

    Thu Jun  6 12:13:22 2024

--- homes/aarch64-darwin/yaro@gwyn/default.nix
+++ homes/aarch64-darwin/yaro@gwyn/default.nix
@@ -22,4 +22,74 @@ with lib.united;
       pyarrow
     ]))
   ];
+
+  accounts = {
+    email.accounts = {
+      Personal = {
+        address = "yarokasear@gmail.com";
+        flavor = "gmail.com";
+        primary = true;
+        realName = "Yaro Kasear";
+        thunderbird = {
+          enable = true;
+          settings = id: {
+            "mail.smtpserver.smtp_${id}.authMethod" = 10;
+            "mail.server.server_${id}.authMethod" = 10;
+          };
+        };
+        gpg = {
+          key = "8A676FDCAAD929184299D020151A8F0401FB2E85";
+          signByDefault = true;
+        };
+      };
+      Heartbeat = {
+        address = "yaro@kasear.net";
+        flavor = "plain";
+        realName = "Yaro Kasear";
+        userName = "yaro@kasear.net";
+        thunderbird = enabled;
+        imap = {
+          host = "127.0.0.1";
+          port = 1143;
+          tls = {
+            enable = true;
+            useStartTls = true;
+          };
+        };
+        smtp = {
+          host = "127.0.0.1";
+          port = 1025;
+          tls = {
+            enable = true;
+            useStartTls = true;
+          };
+        };
+      };
+      Wanachi = {
+        address = "wanachi@tlkmuck.org";
+        flavor = "gmail.com";
+        realName = "Wanachi";
+        thunderbird = {
+          enable = true;
+          settings = id: {
+            "mail.smtpserver.smtp_${id}.authMethod" = 10;
+            "mail.server.server_${id}.authMethod" = 10;
+          };
+        };
+        gpg = {
+            key = "8A676FDCAAD929184299D020151A8F0401FB2E85";
+            signByDefault = true;
+        };
+      };
+    };
+    calendar.accounts = {
+      Personal = {
+        primary = true;
+        remote = {
+          type = "google_calendar";
+          userName = "yarokasear@gmail.com";
+        };
+      };
+    };
+  };
 }
