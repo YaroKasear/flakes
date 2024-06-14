7e9a475220bcbb58a9cf08441b5cac2d59802119
Yaro Kasear <yarokasear@gmail.com>
2024-06-14 10:26:14 -0500

    Fri Jun 14 10:26:14 2024

--- modules/home/gui/plasma/default.nix
+++ modules/home/gui/plasma/default.nix
@@ -12,7 +12,10 @@ in {
 
   config = mkIf cfg.enable {
     home = {
-      file.".face.icon".source = config.united.user.icon;
+      file = {
+        ".face.icon".source = config.united.user.icon;
+        ".mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = "${pkgs.kdePackages.plasma-browser-integration}/lib/mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+      };
       packages = with pkgs; [
         aha
         inputs.plasma-manager.packages.${pkgs.system}.rc2nix
@@ -36,88 +39,93 @@ in {
       };
     };
 
-    programs.plasma = {
-      enable = true;
-      overrideConfig = true;
-
-      fonts = rec {
-        general = {
-          family = config.united.style.fonts.interface;
-          pointSize = 10;
-        };
-        fixedWidth = {
-          family = config.united.style.fonts.terminal;
-          pointSize = 10;
-        };
-        small = {
-          family = config.united.style.fonts.interface;
-          pointSize = 8;
-        };
-        toolbar = general;
-        menu = general;
-        windowTitle = general;
+    programs = {
+      firefox.package = pkgs.firefox.override {
+        cfg.nativeMessagingHosts.packages = [pkgs.kdePackages.plasma-browser-integration];
       };
+      plasma = {
+        enable = true;
+        overrideConfig = true;
 
-      workspace = {
-        clickItemTo = "select";
-        wallpaper = "${config.united.style.wallpaper}";
-        colorScheme = mkDefault "BreezeDark";
-      };
-
-      configFile = {
-        kdeglobals = {
-          KDE.widgetStyle = "Fusion";
-          General = {
-            TerminalApplication = "kitty";
-            TerminalService = "kitty.desktop";
-            XftAntialias = true;
-            XftHintStyle = "hintfull";
-            XftSubPixel = "rgb";
+        fonts = rec {
+          general = {
+            family = config.united.style.fonts.interface;
+            pointSize = 10;
           };
+          fixedWidth = {
+            family = config.united.style.fonts.terminal;
+            pointSize = 10;
+          };
+          small = {
+            family = config.united.style.fonts.interface;
+            pointSize = 8;
+          };
+          toolbar = general;
+          menu = general;
+          windowTitle = general;
         };
-        kwinrc = {
-          Desktops = {
-            Id_1 = "c698a291-b9e2-4d58-804f-7939e0b0d748";
-            Id_2 = "19f5a1ed-4506-4f91-9d50-8117011b1d43";
-            Id_3 = "88ea78bd-94d7-4898-82f5-39310744f518";
-            Id_4 = "682dae2d-0911-44e6-8e80-ad6580b91323";
-            Number = 4;
-            Rows = 2;
-          };
-          Effect-blur.BlurStrength = 3;
-          Effect-overview.BorderActivate = 9;
-          MouseBindings.CommandTitlebarWheel = "Shade/Unshade";
-          NightColor = {
-            Active = true;
-            LatitudeFixed = 42.0327;
-            LongitudeFixed = -97.42;
-            Mode = "Location";
-          };
-          Xwayland.Scale = 1;
 
+        workspace = {
+          clickItemTo = "select";
+          wallpaper = "${config.united.style.wallpaper}";
+          colorScheme = mkDefault "BreezeDark";
         };
-        ksmserverrc.General.loginMode = "emptySession";
-        kwinrulesrc = {
-          "1" = {
-            Description = "Window settings for firefox";
-            above = true;
-            aboverule = 3;
-            desktopsrule = 3;
-            skippager = true;
-            skippagerrule = 3;
-            skipswitcher = true;
-            skipswitcherrule = 3;
-            skiptaskbar = true;
-            skiptaskbarrule = 3;
-            title = "Picture-in-Picture";
-            titlematch = 1;
-            types = 1;
-            wmclass = "firefox";
-            wmclassmatch = 1;
+
+        configFile = {
+          kdeglobals = {
+            KDE.widgetStyle = "Fusion";
+            General = {
+              TerminalApplication = "kitty";
+              TerminalService = "kitty.desktop";
+              XftAntialias = true;
+              XftHintStyle = "hintfull";
+              XftSubPixel = "rgb";
+            };
           };
-          General = {
-            count = 1;
-            rules = "1";
+          kwinrc = {
+            Desktops = {
+              Id_1 = "c698a291-b9e2-4d58-804f-7939e0b0d748";
+              Id_2 = "19f5a1ed-4506-4f91-9d50-8117011b1d43";
+              Id_3 = "88ea78bd-94d7-4898-82f5-39310744f518";
+              Id_4 = "682dae2d-0911-44e6-8e80-ad6580b91323";
+              Number = 4;
+              Rows = 2;
+            };
+            Effect-blur.BlurStrength = 3;
+            Effect-overview.BorderActivate = 9;
+            MouseBindings.CommandTitlebarWheel = "Shade/Unshade";
+            NightColor = {
+              Active = true;
+              LatitudeFixed = 42.0327;
+              LongitudeFixed = -97.42;
+              Mode = "Location";
+            };
+            Xwayland.Scale = 1;
+
+          };
+          ksmserverrc.General.loginMode = "emptySession";
+          kwinrulesrc = {
+            "1" = {
+              Description = "Window settings for firefox";
+              above = true;
+              aboverule = 3;
+              desktopsrule = 3;
+              skippager = true;
+              skippagerrule = 3;
+              skipswitcher = true;
+              skipswitcherrule = 3;
+              skiptaskbar = true;
+              skiptaskbarrule = 3;
+              title = "Picture-in-Picture";
+              titlematch = 1;
+              types = 1;
+              wmclass = "firefox";
+              wmclassmatch = 1;
+            };
+            General = {
+              count = 1;
+              rules = "1";
+            };
           };
         };
       };
