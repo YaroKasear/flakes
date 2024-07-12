8f743c3fada52162f671a478c8d721abf2eb8ee7
Yaro Kasear <yarokasear@gmail.com>
2024-07-12 14:07:32 -0500

    Fri Jul 12 14:07:32 2024

--- containers/home-assistant/default.nix
+++ containers/home-assistant/default.nix
@@ -1,13 +1,17 @@
 { ... }:
 
 {
-  networking.firewall.allowedTCPPorts = [ 8123 ];
 
   services.home-assistant = {
     enable = true;
     config = {
       default_config = {};
     };
+    extraComponents = [
+      "esphome"
+      "met"
+      "radio_browser"
+    ];
   };
 
   system.stateVersion = "24.05";
