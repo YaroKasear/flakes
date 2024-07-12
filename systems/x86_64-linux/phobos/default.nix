4c95139af47ac87e47a8d086df0a4492af1ab7d6
Yaro Kasear <yarokasear@gmail.com>
2024-07-12 12:33:40 -0500

    Fri Jul 12 12:33:40 2024

--- systems/x86_64-linux/phobos/default.nix
+++ systems/x86_64-linux/phobos/default.nix
@@ -43,7 +43,7 @@ in {
       internalInterfaces = ["ve-webtest"];
       externalInterface = config.systemd.network.networks."30-main".matchConfig.Name;
     };
-    networking.firewall.extraCommands = ''
+    firewall.extraCommands = ''
       iptables -w -t nat -A nixos-nat-post -s ${config.containers.webtest.localAddress} -j MASQUERADE
     '';
   };
