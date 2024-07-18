5b3aad148579940760e2441f98130ba8464ee2e5
Yaro Kasear <yarokasear@gmail.com>
2024-07-18 15:58:10 -0500

    Thu Jul 18 15:58:10 2024

--- containers/nodered/default.nix
+++ containers/nodered/default.nix
@@ -4,24 +4,25 @@
   services.node-red = {
     enable = true;
     openFirewall = true;
-    package = pkgs.nodePackages_latest.node-red.overrideAttrs (oldAttrs: {
-      nativeBuildInputs = oldAttrs.nativeBuildInputs or [] ++ [ pkgs.makeWrapper ];
-      postInstall = oldAttrs.postInstall or "" + ''
-        mkdir -p $out/bin
-        wrapProgram $out/bin/node-red \
-          --prefix PATH : "${pkgs.lib.makeBinPath [
-            # module installation
-            pkgs.nodejs
-            pkgs.nodePackages.npm
-            pkgs.bash
-            pkgs.gcc
-            pkgs.python3
-            # projects
-            pkgs.git
-            pkgs.openssl
-            pkgs.openssh ]}"
-      '';
-    });
+    withNpmAndGcc = true;
+    # package = pkgs.nodePackages_latest.node-red.overrideAttrs (oldAttrs: {
+    #   nativeBuildInputs = oldAttrs.nativeBuildInputs or [] ++ [ pkgs.makeWrapper ];
+    #   postInstall = oldAttrs.postInstall or "" + ''
+    #     mkdir -p $out/bin
+    #     wrapProgram $out/bin/node-red \
+    #       --prefix PATH : "${pkgs.lib.makeBinPath [
+    #         # module installation
+    #         pkgs.nodejs
+    #         pkgs.nodePackages.npm
+    #         pkgs.bash
+    #         pkgs.gcc
+    #         pkgs.python3
+    #         # projects
+    #         pkgs.git
+    #         pkgs.openssl
+    #         pkgs.openssh ]}"
+    #   '';
+    # });
   };
 
   system.stateVersion = "24.05";
