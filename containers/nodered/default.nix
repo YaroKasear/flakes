bbaa845b6f4a5b89bf5dca8ca6bf6d6c4b8b99d5
Yaro Kasear <yarokasear@gmail.com>
2024-07-18 13:04:55 -0500

    Thu Jul 18 13:04:55 2024

--- containers/nodered/default.nix
+++ containers/nodered/default.nix
@@ -1,10 +1,27 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.node-red = {
     enable = true;
     openFirewall = true;
     withNpmAndGcc = true;
+    package = pkgs.nodePackages.node-red.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = oldAttrs.nativeBuildInputs or [] ++ [ pkgs.makeWrapper ];
+    postInstall = oldAttrs.postInstall or "" + ''
+      mkdir -p $out/bin
+      wrapProgram $out/bin/node-red \
+        --prefix PATH : "${pkgs.lib.makeBinPath [
+          # module installation
+          pkgs.nodejs
+          pkgs.nodePackages.npm
+          pkgs.bash
+          pkgs.gcc
+          # projects
+          pkgs.git
+          pkgs.openssl
+          pkgs.openssh ]}"
+    '';
+  });
   };
 
   system.stateVersion = "24.05";
