6016593f60c197bbc3a442d9182e40cbd61bbbe3
Yaro Kasear <yarokasear@gmail.com>
2024-07-18 16:18:40 -0500

    Thu Jul 18 16:18:40 2024

--- flake.nix
+++ flake.nix
@@ -37,6 +37,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    hass-node-red = {
+      url = "github:zachowj/hass-node-red";
+      flake = false;
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
