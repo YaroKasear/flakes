e3c69949f381b1268ba4c3b10fdcc4fa5c22384d
Yaro Kasear <yarokasear@gmail.com>
2024-07-27 10:51:48 -0500

    Sat Jul 27 10:51:48 2024

--- containers/home-assistant/default.nix
+++ containers/home-assistant/default.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }:
+with pkgs;
 
 {
   services.home-assistant = {
@@ -7,23 +8,35 @@
       default_config = {};
     };
     configDir = "/etc/hass";
+    configWritable = true;
     extraComponents = [
       "esphome"
       "google_translate"
       "met"
       "radio_browser"
     ];
+    homeassistant = {
+      name = "Home";
+      latitude = -97.429167;
+      longitude = 42.028056;
+      unit_system = "imperial";
+    };
   };
 
-  environment.etc."hass/custom_components/nodered" = {
-    user = "hass";
-    group = "hass";
-    source = pkgs.fetchFromGitHub {
-      owner = "zachowj";
-      repo = "hass-node-red";
-      rev = "v4.0.1";
-      sha256 = "ePphcSWSWhI51iNJsKryuo52ck7S5LuNREfvndIuVfs=";
-    } + "/custom_components/nodered";
+  environment = {
+    etc."hass/custom_components/nodered" = {
+      user = "hass";
+      group = "hass";
+      source = fetchFromGitHub {
+        owner = "zachowj";
+        repo = "hass-node-red";
+        rev = "v4.0.1";
+        sha256 = "ePphcSWSWhI51iNJsKryuo52ck7S5LuNREfvndIuVfs=";
+      } + "/custom_components/nodered";
+    };
+    systemPackages =  [
+      home-assistant-cli
+    ];
   };
 
   system.stateVersion = "24.05";
