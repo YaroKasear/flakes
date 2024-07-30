23f3d2e5d4317e5ee9f6a21e048414c33f840f0c
Yaro Kasear <yarokasear@gmail.com>
2024-07-29 19:27:51 -0500

    Mon Jul 29 19:27:51 2024

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -105,45 +105,6 @@ in {
           Defaults insults
         '';
       };
-      pam = {
-        u2f = {
-          enable = mkDefault true;
-          authFile = "${config.age.secrets.yubikey-auth.path}";
-          cue = true;
-          control = "sufficient";
-        };
-        yubico = {
-          enable = mkDefault true;
-          id = "65698";
-          control = "sufficient";
-        };
-        services = {
-          kde = { # Taking off multifactor as Plasma's screensaver lock thing doesn't seem to play nice with it.
-            u2fAuth = false;
-            yubicoAuth = false;
-          };
-          login = with config.security.pam.services.login.rules.auth; {
-            rules.auth = {
-              unix = {
-                control = mkForce "required";
-                order = u2f.order - 30;
-              };
-              yubico.order = u2f.order + 10;
-            };
-	          u2fAuth = true;
-          };
-          sudo = with config.security.pam.services.sudo.rules.auth; {
-            rules.auth = {
-              unix = {
-                control = mkForce "required";
-                order = u2f.order - 10;
-              };
-              yubico.order = u2f.order + 10;
-            };
-            u2fAuth = true;
-          };
-        };
-      };
     };
 
     services = {
@@ -167,6 +128,8 @@ in {
     systemd.enableEmergencyMode = false;
 
     time.timeZone = "America/Chicago";
+
+    united.pam.enable = mkDefault true;
 
     users = {
       mutableUsers = false;
---
+++ modules/nixos/pam/default.nix
@@ -0,0 +1,53 @@
+{ lib, config, ... }:
+
+with lib;
+with lib.united;
+let
+  cfg = config.united.pam;
+in {
+  options.united.pam = {
+    enable = mkEnableOption "PAM";
+  };
+
+  config = mkIf cfg.enable {
+    security.pam = {
+      u2f = {
+        enable = mkDefault true;
+        authFile = "${config.age.secrets.yubikey-auth.path}";
+        cue = true;
+        control = "sufficient";
+      };
+      yubico = {
+        enable = mkDefault true;
+        id = "65698";
+        control = "sufficient";
+      };
+      services = {
+        kde = { # Taking off multifactor as Plasma's screensaver lock thing doesn't seem to play nice with it.
+          u2fAuth = false;
+          yubicoAuth = false;
+        };
+        login = with config.security.pam.services.login.rules.auth; {
+          rules.auth = {
+            unix = {
+              control = mkForce "required";
+              order = u2f.order - 30;
+            };
+            yubico.order = u2f.order + 10;
+          };
+	        u2fAuth = true;
+        };
+        sudo = with config.security.pam.services.sudo.rules.auth; {
+          rules.auth = {
+            unix = {
+              control = mkForce "required";
+              order = u2f.order - 10;
+            };
+            yubico.order = u2f.order + 10;
+          };
+          u2fAuth = true;
+        };
+      };
+    };
+  };
+}
