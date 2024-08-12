1df2c71d5acd3bf5222894da09bfaf2aa3cfbfa6
Yaro Kasear <yarokasear@gmail.com>
2024-08-12 14:03:22 -0500

    Mon Aug 12 14:03:22 2024

--- modules/nixos/emby/emby-server.nix
+++ modules/nixos/emby/emby-server.nix
@@ -6,12 +6,6 @@ stdenv.mkDerivation rec {
   name = "emby-${version}";
   version = "4.8.8.0";
 
-  # We are fetching a binary here, however, a source build is possible.
-  # See -> https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=emby-server-git#n43
-  # Though in my attempt it failed with this error repeatedly
-  # The type 'Attribute' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.
-  # This may also need msbuild (instead of xbuild) which isn't in nixpkgs
-  # See -> https://github.com/NixOS/nixpkgs/issues/29817
   src = fetchurl {
     url = "https://github.com/MediaBrowser/Emby.Releases/releases/download/${version}/embyserver-netcore_${version}.zip";
     sha256 = "aglGe0s77UDLWbnJb1VxzhJcY0eq7mN7RFXwOXVEFBU=";
