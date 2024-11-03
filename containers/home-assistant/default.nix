b1fd432848b22da5df9c27f46c4183736cc2afa1
Yaro Kasear <yarokasear@gmail.com>
2024-11-03 07:51:35 -0600

    Updates 11-03-24

--- containers/home-assistant/default.nix
+++ containers/home-assistant/default.nix
@@ -6,7 +6,7 @@ with pkgs;
   services.home-assistant = {
     enable = true;
     config = {
-      default_config = {};
+      default_config = { };
     };
     configDir = "/etc/hass";
     configWritable = true;
@@ -15,6 +15,7 @@ with pkgs;
       "google_translate"
       "tplink"
       "met"
+      "minecraft_server"
       "radio_browser"
       "xiaomi_miio"
     ];
@@ -24,14 +25,15 @@ with pkgs;
     etc."hass/custom_components/nodered" = {
       user = "hass";
       group = "hass";
-      source = fetchFromGitHub {
-        owner = "zachowj";
-        repo = "hass-node-red";
-        rev = "v4.0.1";
-        sha256 = "ePphcSWSWhI51iNJsKryuo52ck7S5LuNREfvndIuVfs=";
-      } + "/custom_components/nodered";
+      source = fetchFromGitHub
+        {
+          owner = "zachowj";
+          repo = "hass-node-red";
+          rev = "v4.0.1";
+          sha256 = "ePphcSWSWhI51iNJsKryuo52ck7S5LuNREfvndIuVfs=";
+        } + "/custom_components/nodered";
     };
-    systemPackages =  [
+    systemPackages = [
       home-assistant-cli
     ];
   };
