2e837cd46f449aa13ad032deec5b79523d585cb7
Yaro Kasear <yarokasear@gmail.com>
2024-11-19 11:50:57 -0600

    Backporting Tailscale stuff from unstable.

--- modules/nixos/tailscale/default.nix
+++ modules/nixos/tailscale/default.nix
@@ -1,33 +1,38 @@
-{ config, lib, ... }:
+{ config, lib, inputs, ... }:
 with lib;
+with lib.united;
 
 let
   cfg = config.united.tailscale;
+  secrets-directory = inputs.self + "/secrets/modules/tailscale/";
 
 in
 {
-  options.united.headscale = {
+  options.united.tailscale = {
     enable = mkEnableOption "Tailscale";
-    exitNode = mkEnableOption "Whether or not this node should be used as an exit point.";
+    router = mkEnableOption "Whether or not this node should be used as an exit point.";
+    accept-routes = mkOpt types.bool false "Whether to accept routes from a Tailscale router.";
   };
 
   config = mkIf cfg.enable {
-    age.secrets."tsAuthFile" = { };
+    age.secrets."tsAuthFile-${config.networking.hostName}".rekeyFile = secrets-directory + "tsAuthFile-${config.networking.hostName}.age";
+
     services.tailscale = {
       enable = true;
+      authKeyFile = config.age.secrets."tsAuthFile-${config.networking.hostName}".path;
       extraUpFlags = [
-        "--accept-routes"
-        (mkIf exitNode "--advertise-exit-node")
-        (mkIf exitNode "--advertise-routes=10.0.10.1/32,10.10.10.2/32,10.40.10.1/32")
+        "--accept-routes${if cfg.accept-routes then "" else "=false"}"
+        (mkIf cfg.router "--advertise-routes=10.0.10.1/32,10.10.10.2/32,10.40.10.1/32")
         "--login-server=https://vpn.kasear.net"
-        (mkIf exitNode != true "--shields-up")
-
+        (mkIf (cfg.router != true) "--shields-up")
       ];
       useRoutingFeatures =
-        if cfg.exitNode then
+        if cfg.router then
           "server"
         else
           "client";
     };
+
+    environment.persistence."/persistent".directories = [ "/var/lib/tailscale" ];
   };
 }
