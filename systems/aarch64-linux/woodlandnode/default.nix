ada1c1429fa8961c77cf6d75201fb8348502c1c9
Yaro Kasear <yarokasear@gmail.com>
2024-11-24 12:57:29 -0600

    Preliminary new node.

---
+++ systems/aarch64-linux/woodlandnode/default.nix
@@ -0,0 +1,60 @@
+{ config, inputs, lib, pkgs, ... }:
+with lib;
+with lib.united;
+
+let
+  secrets-directory = inputs.self + "/secrets/${pkgs.system}/${config.networking.hostName}/";
+
+  SSID = "Heartbeat Communications - Main";
+  SSIDpassword = "ext:psk";
+  interface = "wlan0";
+  hostname = "woodlandnode";
+in
+{
+  age = {
+    identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    rekey = {
+      hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN9GDqDSCLAeQB6VT+7wOnG81HLi1yaub+pnpQLOJCDq";
+    };
+    secrets = {
+      wireless-secret.rekeyFile = secrets-directory + "wireless-secret.age";
+      yaro-password.rekeyFile = secrets-directory + "yaro-password.age";
+    };
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  networking = {
+    hostName = hostname;
+    wireless = {
+      enable = true;
+      environmentFile = age.secrets.wireless-secret.path;
+      networks."${SSID}".pskRaw = SSIDpassword;
+      interfaces = [ interface ];
+    };
+  };
+
+  united.common = enabled;
+
+  environment.systemPackages = with pkgs; [ vim ];
+
+  services.openssh.enable = true;
+
+  hardware.enableRedistributableFirmware = true;
+  system.stateVersion = "23.11";
+}
