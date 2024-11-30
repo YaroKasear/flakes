60ec86aef3a485e6d0d1f6d18d4259a55a045a1e
Yaro Kasear <yarokasear@gmail.com>
2024-11-30 11:53:47 -0600

    Sat Nov 30 11:53:47 2024

--- modules/nixos/tailscale/default.nix
+++ modules/nixos/tailscale/default.nix
@@ -13,6 +13,7 @@ in
     router = mkEnableOption "Whether or not this node should be used as an exit point.";
     accept-dns = mkOpt types.bool false "Whether to accept DNS from the tailnet.";
     accept-routes = mkOpt types.bool false "Whether to accept routes from a Tailscale router.";
+    accept-connections = mkOpt types.bool false "Should the node accept connections through the tailnet?";
     local-network = mkEnableOption "Is the node on the same network as the control plane?";
   };
 
@@ -27,7 +28,7 @@ in
         "--accept-routes${if cfg.accept-routes then "" else "=false"}"
         (mkIf cfg.router "--advertise-routes=10.0.10.1/32,10.10.10.1/32,10.40.10.1/32")
         "--login-server=https://vpn.kasear.net"
-        (mkIf (cfg.router != true) "--shields-up")
+        (mkIf (cfg.router != true && cfg.accept-connections != true) "--shields-up")
       ];
       useRoutingFeatures =
         if cfg.router then
