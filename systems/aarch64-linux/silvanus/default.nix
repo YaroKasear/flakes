01b0f00eb5849d22ad6be4d783cf10f62d9b5800
Yaro Kasear <yarokasear@gmail.com>
2024-11-30 22:40:57 -0600

    Sat Nov 30 22:40:57 2024

--- systems/aarch64-linux/silvanus/default.nix
+++ systems/aarch64-linux/silvanus/default.nix
@@ -23,8 +23,6 @@ in
   };
 
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
     loader = {
       grub.enable = false;
       generic-extlinux-compatible.enable = true;
@@ -101,5 +99,8 @@ in
     };
   };
 
-  hardware.enableRedistributableFirmware = true;
+  hardware = {
+    enableRedistributableFirmware = true;
+    raspberry-pi."4".xhci = enabled;
+  };
 }
