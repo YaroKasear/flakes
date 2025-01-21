3e4e182096af0ef10c93f82111e19b12302eeb0c
Yaro Kasear <yarokasear@gmail.com>
2025-01-20 18:21:12 -0600

    Mon Jan 20 18:21:12 2025

--- systems/aarch64-linux/silvanus/default.nix
+++ systems/aarch64-linux/silvanus/default.nix
@@ -23,7 +23,7 @@ in
   };
 
   boot.loader.systemd-boot = disabled;
-  boot.initrd.systemd.enableTpm2 = false;
+  boot.initrd.systemd.tpm2 = disabled;
 
   fileSystems = {
     "/" = {
@@ -93,50 +93,50 @@ in
     };
   };
 
-  systemd = {
-    services.runtest = {
-      path = [
-        pkgs.coreutils
-        pkgs.gawk
-        pkgs.iw
-        pkgs.speedtest-cli
-      ];
-      script =
-        let
-          storage-path = config.users.users.yaro.home;
-          num-tests = 3;
-          samples = 10;
-        in
-        ''
-          #!${pkgs.bash}/bin/bash
-
-          COUNTER_FILE=${storage-path}/$(cat /proc/sys/kernel/random/boot_id)-counter
-          NUM_TESTS=${toString num-tests}
-
-          TEST_FILE=${storage-path}/$(cat /proc/sys/kernel/random/boot_id)-speedtest.csv
-
-          if [ ! -f $COUNTER_FILE ]; then
-            echo 0 > $COUNTER_FILE
-          fi
-
-          COUNTER=$(cat $COUNTER_FILE)
-
-          if [ ! -f $TEST_FILE ]; then
-            echo "Link,Level,Noise,BSSID,TX Bitrate,RX Bitrate,$(speedtest --csv-header)" > $TEST_FILE
-          fi
-
-          for i in {1..${toString samples}}
-          do
-            link_level_noise=$(awk 'NR==3 {gsub(/\./, "", $3); gsub(/\./, "", $4); gsub(/\./, "", $5); print $3","$4","$5}' /proc/net/wireless)
-            bssid_and_bitrate=$(iw dev wlp7s0 link | awk '/Connected/ {bssid=$3} /tx bitrate/ {tx=$3} /rx bitrate/ {rx=$3} END {print bssid","tx","rx}')
-            speed_results=$(speedtest --csv)
-
-            echo "$link_level_noise,$bssid_and_bitrate,$speed_results" >> $(cat /proc/sys/kernel/random/boot_id)-speedtest.csv
-          done
-        '';
-    };
-    timers.runtest = {
-      after = [ "systemd-networkd-wait-online.service" ];
-    };
-  };
+  # systemd = {
+  #   services.runtest = {
+  #     path = [
+  #       pkgs.coreutils
+  #       pkgs.gawk
+  #       pkgs.iw
+  #       pkgs.speedtest-cli
+  #     ];
+  #     script =
+  #       let
+  #         storage-path = config.users.users.yaro.home;
+  #         num-tests = 3;
+  #         samples = 10;
+  #       in
+  #       ''
+  #         #!${pkgs.bash}/bin/bash
+  #
+  #         COUNTER_FILE=${storage-path}/$(cat /proc/sys/kernel/random/boot_id)-counter
+  #         NUM_TESTS=${toString num-tests}
+  #
+  #         TEST_FILE=${storage-path}/$(cat /proc/sys/kernel/random/boot_id)-speedtest.csv
+  #
+  #         if [ ! -f $COUNTER_FILE ]; then
+  #           echo 0 > $COUNTER_FILE
+  #         fi
+  #
+  #         COUNTER=$(cat $COUNTER_FILE)
+  #
+  #         if [ ! -f $TEST_FILE ]; then
+  #           echo "Link,Level,Noise,BSSID,TX Bitrate,RX Bitrate,$(speedtest --csv-header)" > $TEST_FILE
+  #         fi
+  #
+  #         for i in {1..${toString samples}}
+  #         do
+  #           link_level_noise=$(awk 'NR==3 {gsub(/\./, "", $3); gsub(/\./, "", $4); gsub(/\./, "", $5); print $3","$4","$5}' /proc/net/wireless)
+  #           bssid_and_bitrate=$(iw dev wlp7s0 link | awk '/Connected/ {bssid=$3} /tx bitrate/ {tx=$3} /rx bitrate/ {rx=$3} END {print bssid","tx","rx}')
+  #           speed_results=$(speedtest --csv)
+  #
+  #           echo "$link_level_noise,$bssid_and_bitrate,$speed_results" >> $(cat /proc/sys/kernel/random/boot_id)-speedtest.csv
+  #         done
+  #       '';
+  #   };
+  #   timers.runtest = {
+  #     after = [ "systemd-networkd-wait-online.service" ];
+  #   };
+  # };
 }
